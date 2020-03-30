256302bc78e6faedbe694a84d8811809a0475fb0
Fbada006 <ferdinandkamata@yahoo.com>
2020-03-30 15:15:23 +0300

    Add back navigation to SettingsActivity

--- app/src/main/java/com/disruption/popularmovies1/settings/SettingsActivity.java
+++ app/src/main/java/com/disruption/popularmovies1/settings/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.disruption.popularmovies1.settings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.disruption.popularmovies1.R;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,5 +14,6 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setTitle(getString(R.string.settings_label));
     }
 }
