e12dac13de0a867f60245b3e4709345253ad4c4a
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-01 15:48:29 +0300

    Remove logging

--- app/src/main/java/com/disruption/popularmovies/network/MovieApiService.java
+++ app/src/main/java/com/disruption/popularmovies/network/MovieApiService.java
@@ -1,7 +1,5 @@
 package com.disruption.popularmovies.network;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,20 +9,12 @@ import static com.disruption.popularmovies.utils.Constants.BASE_URL;
 public class MovieApiService {
     private static Retrofit retrofit;
 
-    private static HttpLoggingInterceptor mHttpLoggingInterceptor =
-            new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-    private static OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
-            .addInterceptor(mHttpLoggingInterceptor)
-            .build();
-
     private static Retrofit retrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .client(mOkHttpClient)
                     .build();
         }
         return retrofit;
